a004789bb37daecea310331b6ce33b02b283dc9d
Dmitry Mager <magerdima@gmail.com>
2021-09-04 23:27:20 +0300

    commit3

--- src/com/epam/jwd/Main.java
+++ src/com/epam/jwd/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");// write your code here
         System.out.println("commit2");
+        System.out.println("commit3");
     }
 
 }
