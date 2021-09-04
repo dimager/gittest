53310a9ba153595056946008729841f15d289b39
Dmitry Mager <magerdima@gmail.com>
2021-09-04 23:32:31 +0300

    branch commit 3

--- src/com/epam/jwd/Main.java
+++ src/com/epam/jwd/Main.java
@@ -9,6 +9,8 @@ public class Main {
         System.out.println("Branch2");
         System.out.println("commit3");
         System.out.println("Commit4");
+        System.out.println("Branch3");
+
     }
 
 }
